6093a9a3bac4c6d2c9e63848d0e64401e3781a4c
Sachin Mahajan <sachinm78@gmail.com>
2019-05-15 19:32:47 +0000

    Automatically backed up by Learn

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,3 @@
 Rails.application.routes.draw do
-  resources :articles, only: [:index, :show, :new, :create, :edit, :update]
+  resources :posts, only: [:index, :show, :new, :create, :edit, :update]
 end
